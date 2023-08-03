fa036b6d68e1b35e687d328648f07bf36920b799
yannis94 <yannisbgci@proton.me>
2023-08-03 19:44:46 +0200

    upgrade auth unit test

--- api/auth_handler_test.go
+++ api/auth_handler_test.go
@@ -12,15 +12,9 @@ import (
 	customtypes "github.com/yannis94/hotel-reservation/custom_types"
 )
 
-func createTestUser(app *fiber.App) *customtypes.User {
-    params := customtypes.CreateUserParams{
-        FirstName: "Elliot",
-        LastName: "Alderson",
-        Email: "elliot.alderson@fsociety.com",
-        Password: "thesuperpassword",
-    }
+func createTestUser(app *fiber.App, user *customtypes.CreateUserParams) *customtypes.User {
 
-    data, _ := json.Marshal(params)
+    data, _ := json.Marshal(user)
 
     req := httptest.NewRequest("POST", "/api/v1/user", bytes.NewReader(data))
     req.Header.Add("Content-Type", "application/json")
@@ -28,11 +22,11 @@ func createTestUser(app *fiber.App) *customtypes.User {
 
     defer res.Body.Close()
 
-    var user customtypes.User
+    var resUser customtypes.User
 
-    json.NewDecoder(res.Body).Decode(&user)
+    json.NewDecoder(res.Body).Decode(&resUser)
 
-    return &user
+    return &resUser
 }
 
 func TestAuthenticate(t *testing.T) {
@@ -45,7 +39,13 @@ func TestAuthenticate(t *testing.T) {
     app.Post("/api/v1/user", userHandler.HandlePostUser)
     app.Post("/api/auth", authHandler.HandleAuthenticate)
 
-    userTest := createTestUser(app)
+    validUser := &customtypes.CreateUserParams{
+        FirstName: "Elliot",
+        LastName: "Alderson",
+        Email: "elliot.alderson@fsociety.com",
+        Password: "thesuperpassword",
+    }
+    userTest := createTestUser(app, validUser)
     fmt.Println(userTest)
 
 
@@ -87,5 +87,31 @@ func TestAuthenticate(t *testing.T) {
         t.Error("The authentication should provide a JWT token")
     }
     
-    fmt.Println(authResp)
+    wrongParams := AuthParams{
+        Email: "wrongemail@mail.com",
+        Password: "thesuperpassword",
+    }
+
+    wrongData, _ := json.Marshal(wrongParams)
+
+    reqWrong := httptest.NewRequest("POST", "/api/auth", bytes.NewReader(wrongData))
+    reqWrong.Header.Add("Content-Type", "application/json")
+    resWrong, err := app.Test(reqWrong)
+
+    if err != nil {
+        t.Fatal(err)
+    }
+
+
+    var authWrongResponse AuthResponse
+    
+    if err := json.NewDecoder(resWrong.Body).Decode(&authWrongResponse); err != nil {
+        t.Error(err)
+    }
+
+    defer resWrong.Body.Close()
+
+    if resWrong.StatusCode != http.StatusUnauthorized {
+        t.Errorf("Expect http status 401 but got %d", res.StatusCode)
+    }
 }
